30eb0e52a34e62456167e54a29b1886ef885bf11
Long Vo / 武黃隆 <144998835+yodawu28@users.noreply.github.com>
2026-08-24 19:32:52 +0700

    Add: k-closest-points-to-origin - submission-11

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-11.cpp
@@ -0,0 +1,33 @@
+struct CompareByPriority {
+    bool operator()(const vector<int>& a, const vector<int>& b) {
+        double a_len = a[0] * a[0] + a[1] * a[1];
+        double b_len = b[0] * b[0] + b[1] * b[1];
+        return a_len < b_len; // min heap by second
+    }
+};
+
+class Solution {
+public:
+    priority_queue<vector<int>, vector<vector<int>>, CompareByPriority> pq; 
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        for (int i = 0; i < points.size(); i++) {
+            pq.push(points[i]);
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }   
+
+        vector<vector<int>> ans;
+        while (k > 0) {
+            ans.push_back(pq.top());
+            pq.pop();
+            k--;
+        } 
+
+        return ans;
+    }
+
+    double getDistance(vector<int> v) {
+        return 1.0 * v[0] * v[0] + v[1] * v[1];
+    }
+};
