f56a8c84205868d5f7fd52a54e46962899a5fedb
Long Vo / 武黃隆 <144998835+yodawu28@users.noreply.github.com>
2026-08-24 18:18:22 +0700

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    priority_queue<int> pq;
+    int lastStoneWeight(vector<int>& stones) {
+        for (int i = 0; i < stones.size(); i++) {
+            pq.push(stones[i]);
+        }
+
+        while (pq.size() >= 2) {
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+
+            y = abs(y - x);
+            pq.push(y);
+        }
+
+        return pq.top();
+    }
+};
